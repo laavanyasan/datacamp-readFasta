8cc135060e2f512b120be0816fbab9ccec466f9a
Laavanya Sankar <ls331@duke.edu>
2018-08-16 10:32:13 -0400

    Defensive prog is tedious

--- read_fasta.py
+++ read_fasta.py
@@ -1,5 +1,3 @@
-import sys
-
 def read_fasta(filename):
     sequence = ''
     f = open(filename)
@@ -11,8 +9,4 @@ def read_fasta(filename):
     f.close()
     return sequence
 
-if len(sys.argv) < 2:
-    print('Usage:', sys.argv[0], '<sequence.fa>')
-    exit(1)
-
-print(read_fasta(sys.argv[1]))
+print(read_fasta('ae.fa'))
