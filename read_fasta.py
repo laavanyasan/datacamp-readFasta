1782b71664bc5b489b60ab2233e8e5b91dfba46a
Laavanya Sankar <ls331@duke.edu>
2018-08-16 10:44:35 -0400

    Revert "Trial to commit"
    This reverts commit 43461b54fed6eda5b2578aaa915dbda7b6f47868.

--- read_fasta.py
+++ read_fasta.py
@@ -9,8 +9,4 @@ def read_fasta(filename):
     f.close()
     return sequence
 
-<<<<<<< HEAD
 print(read_fasta('ae.fa'))
-=======
-print(read_fasta(sys.argv[1]))
->>>>>>> parent of 75a514b... There is adding an arguement to check for whether there is a file name added or not.
